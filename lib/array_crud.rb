05b2921f2a609ab98e7ff7058ce89c1ab2f55376
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-02-14 17:53:02 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-[1, 2, 3, 4]
+num =[1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-
+num =[1, 2, 3, 4]
+num << 5
 end
 
 def add_element_to_start_of_array(array, element)
